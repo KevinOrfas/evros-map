fd2eb67887ce6405a39fa1f15d788754b62ba566
Anastasios Orfanidis <anastasios@kopernio.com>
2020-02-02 22:14:38 +0000

    Added data

---
+++ data/cementification_points.js
@@ -0,0 +1,79 @@
+var jsonCementificationPoints = {
+  type: "FeatureCollection",
+  name: "cementification_points",
+  crs: { type: "name", properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+  features: [
+    {
+      type: "Feature",
+      properties: { id: "9" },
+      geometry: {
+        type: "Point",
+        coordinates: [25.524782895084623, 40.474438509573503]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: "7" },
+      geometry: {
+        type: "Point",
+        coordinates: [25.723597455262325, 40.849585846598671]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: "8" },
+      geometry: {
+        type: "Point",
+        coordinates: [25.728272215099839, 40.847435500625316]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: "3" },
+      geometry: {
+        type: "Point",
+        coordinates: [25.732360727746649, 40.847620468864726]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: "4" },
+      geometry: {
+        type: "Point",
+        coordinates: [25.739898529408624, 40.847805445592627]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: "1" },
+      geometry: {
+        type: "Point",
+        coordinates: [25.745077877789857, 40.855227636799661]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: "2" },
+      geometry: {
+        type: "Point",
+        coordinates: [25.755737161735169, 40.851597468514598]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: "5" },
+      geometry: {
+        type: "Point",
+        coordinates: [25.943552445900558, 40.923643448843436]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: "6" },
+      geometry: {
+        type: "Point",
+        coordinates: [25.890083102529701, 40.899138264664053]
+      }
+    }
+  ]
+};
---
+++ data/overgrazing_points.js
@@ -0,0 +1,119 @@
+var jsonOvergrazingPoints = {
+  type: "FeatureCollection",
+  name: "overgrazingPoints",
+  crs: { type: "name", properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+  features: [
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.543319152551067, 40.444945434616521]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.54061382575723, 40.458077541646531]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.522407665418168, 40.467170445524346]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.510626656155765, 40.481691224345909]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.529374508072625, 40.478716617352056]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.521233478412217, 40.491241278328815]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.560490462664472, 40.505527219721174]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.535329984352497, 40.490283141754581]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.512785594648129, 40.447336079300918]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.527044921167317, 40.455057532779477]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.554549076723163, 40.434767581987018]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.685238468345787, 40.456198451629099]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.694592824527778, 40.450758052013164]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.69627582583551, 40.458311988160162]
+      }
+    }
+  ]
+};
