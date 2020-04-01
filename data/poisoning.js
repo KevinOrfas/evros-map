96ce0ba33dedc9da07259cd715b6f5d5844c64a1
Anastasios Orfanidis <anastasios@kopernio.com>
2020-04-01 20:50:03 +0100

    data for poisoning

---
+++ data/poisoning.js
@@ -0,0 +1,79 @@
+var jsonPoisoning = {
+  type: "FeatureCollection",
+  name: "poisoning_12",
+  crs: { type: "name", properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+  features: [
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [26.233260798986525, 41.018217422308872]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.920975561936238, 40.904885271448194]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.933048082646724, 40.912767917315222]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.733517097877666, 40.849900061567681]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.745106323245366, 40.849864835950598]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.750073134120885, 40.847610396972335]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.761239652169159, 40.854444165106933]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.500081615748172, 40.439267606587542]
+      }
+    },
+    {
+      type: "Feature",
+      properties: { id: null },
+      geometry: {
+        type: "Point",
+        coordinates: [25.480042158136538, 40.461394507650454]
+      }
+    }
+  ]
+};
