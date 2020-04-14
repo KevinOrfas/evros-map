28dec9a19c68f64e827c85c369a8c00d8770c20f
Anastasios Orfanidis <anastasios@kopernio.com>
2020-04-14 13:13:16 +0100

    Linted webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
-const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
-const webpack = require("webpack");
-const merge = require("webpack-merge");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const babelLoader = require("./config/babel-loader");
+const path = require('path');
+// const CopyPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const babelLoader = require('./config/babel-loader');
 // const codeGenConfig = require("./codegen-loader");
 
 module.exports = (env) => {
-  const isDevelopement = env === "development";
-  console.log(`This is ${isDevelopement ? "development" : "production"} build`);
+  const isDevelopement = env === 'development';
+  console.log(`This is ${isDevelopement ? 'development' : 'production'} build`);
 
   const baseConfig = {
-    entry: "./src/app.js",
-    mode: "production",
+    entry: './src/app.js',
+    mode: 'production',
     output: {
-      filename: "bundle.js",
-      path: path.resolve(__dirname, "dist"),
-      publicPath: "/dist/",
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/dist/',
     },
     module: {
       rules: [
         {
-          enforce: "pre",
+          enforce: 'pre',
           test: /\.js$/,
           exclude: /node_modules/,
-          loader: "eslint-loader",
+          loader: 'eslint-loader',
         },
       ],
     },
@@ -34,21 +34,21 @@ module.exports = (env) => {
       new webpack.DefinePlugin({
         ENV_IS_DEVELOPMENT: isDevelopement,
         ENV_IS: JSON.stringify(env),
-        "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
       }),
     ],
-    devtool: "none",
+    devtool: 'none',
   };
 
   const devConfig = {
-    mode: "development",
+    mode: 'development',
     devServer: {
-      contentBase: path.resolve(__dirname, "./"),
-      publicPath: "/dist/",
+      contentBase: path.resolve(__dirname, './'),
+      publicPath: '/dist/',
       watchContentBase: false,
       hotOnly: true,
       overlay: true,
-      host: "0.0.0.0",
+      host: '0.0.0.0',
     },
     plugins: [
       new webpack.HotModuleReplacementPlugin(),
@@ -59,7 +59,7 @@ module.exports = (env) => {
       //   },
       // ]),
     ],
-    devtool: "source-map",
+    devtool: 'source-map',
   };
 
   if (isDevelopement) {
