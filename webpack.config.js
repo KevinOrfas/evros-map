356b396da5c00740ee5316e3c9b5357a36c5d7e0
Anastasios Orfanidis <anastasios@kopernio.com>
2020-04-25 13:12:02 +0100

    clean up imports

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
 const merge = require('webpack-merge');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const babelLoader = require('./config/babel-loader');
-// const codeGenConfig = require("./codegen-loader");
 
 module.exports = (env) => {
   const isDevelopement = env === 'development';
